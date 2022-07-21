31e7802f3e135fdcbee036fb3ee37c7a5ad8a30b
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-21 11:51:34 -0700

    Add working C++ code

---
+++ 26. Remove Duplicates from Sorted Array/removeDuplicates.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int removeDuplicates(vector<int> &nums) {
+
+  for (int i = 1; i < nums.size();) {
+    if (nums[i] == nums[i - 1])
+      nums.erase(nums.begin() + i);
+    else
+      i++;
+  }
+  return nums.size();
+}
+
+int main() {
+  vector<int> nums{};
+  cout << removeDuplicates(nums);
+}
+
+/*
+[1,1,2] = 2;
+[0,0,1,1,1,2,2,3,3,4] = 5
+
+*/
