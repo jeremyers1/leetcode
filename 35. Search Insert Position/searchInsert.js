774733f31c9e44185edc23f71008543cc001ed7c
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-22 17:56:37 -0700

    Add BETTER JS Code. MUCH better

--- 35. Search Insert Position/searchInsert.js
+++ 35. Search Insert Position/searchInsert.js
@@ -8,6 +8,18 @@ var searchInsert = function (nums, target) {
 	}
 };
 
+/* Wow, I made this difficult. LOL
+check out this other solution from someone else:
+
+var searchInsert = function (nums, target) {
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] >= target) return i;
+	}
+	return nums.length;
+};
+
+*/
+
 const arr = [1, 3, 5, 6];
 const search = 2;
 console.log(searchInsert(arr, search));
