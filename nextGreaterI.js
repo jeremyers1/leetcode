c94d19dfb08e6b1c2259d6b8fed5d2f02009c0ee
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-24 10:36:34 -0700

    Refactor JS

--- nextGreaterI.js
+++ nextGreaterI.js
@@ -2,15 +2,14 @@ var nextGreaterElement = function (nums1, nums2) {
 	const ans = [];
 	for (let i = 0; i < nums1.length; i++) {
 		let index = nums2.indexOf(nums1[i]);
-		let temp = 0;
+		let temp = -1;
 		for (let j = index; j < nums2.length; j++) {
 			if (nums2[j] > nums1[i]) {
-				ans.push(nums2[j]);
 				temp = nums2[j];
 				break;
 			}
 		}
-		if (temp === 0) ans.push(-1);
+		ans.push(temp);
 	}
 	return ans;
 };
