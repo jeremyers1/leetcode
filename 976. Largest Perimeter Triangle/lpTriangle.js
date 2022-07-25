cda8118f889c6c45f5f4c6e92ef62e137b5f3c5a
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-07-24 17:28:36 -0700

    Refactor to make more readable

--- 976. Largest Perimeter Triangle/lpTriangle.js
+++ 976. Largest Perimeter Triangle/lpTriangle.js
@@ -1,19 +1,18 @@
 var largestPerimeter = function(nums) {
   // find 3 largest numbers
-  nums.sort((a, b) => a - b);
+  nums.sort((a, b) => b-a);
   // make sure 2 of them are greater than or equal to the third
-  for (let i = nums.length - 1; i > 1; i--){
-    if (nums[i-1] + nums[i - 2] > nums[i]) {
-      return nums[i] + nums[i - 1] + nums[i - 2];
+  for (let i = 0; i < nums.length - 2; i++){
+    if (nums[i+1] + nums[i + 2] > nums[i]) {
+      return nums[i] + nums[i + 1] + nums[i + 2];
     }
   }
-
 
   return 0;
 };
 
 
-const arr = [1,1,2];
+const arr = [2,1,2];
 console.log(largestPerimeter(arr));
 
 
