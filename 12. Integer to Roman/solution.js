15f60b5e6e0934f1d00ae85a723e7e82826fce33
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-07 18:48:48 -0700

    Add other solutions

--- 12. Integer to Roman/solution.js
+++ 12. Integer to Roman/solution.js
@@ -18,6 +18,63 @@ var intToRoman = function (num) {
 	}
 
 	return I[numStr[0]];
+
+	/* Another approach:
+  let res = "";
+  while (num > 0) {
+    if (num >= 1000) {
+      res += "M";
+      num -= 1000;
+    }else if (num >= 900) {
+      res += "CM";
+      num -= 900;
+    }else if (num >= 500) {
+      res += "D";
+      num -= 500;
+    }else if (num >= 400) {
+      res += "CD";
+      num -= 400;
+    }else if (num >= 100) {
+      res += "C";
+      num -= 100;
+    }else if (num >= 90) {
+      res += "XC";
+      num -= 90;
+    }else if (num >= 50) {
+      res += "L";
+      num -= 50;
+    }else if (num >= 40) {
+      res += "XL";
+      num -= 40;
+    }else if (num >= 10) {
+      res += "X";
+      num -= 10;
+    }else if (num >= 9) {
+      res += "IX";
+      num -= 9;
+    }else if (num >= 5) {
+      res += "V";
+      num -= 5;
+    }else if (num >= 4) {
+      res += "IV";
+      num -= 4;
+    }else if (num >= 1) {
+      res += "I";
+      num -= 1;
+    }
+  }
+    return res;
+  */
+
+	/* I LOVE this one ... very similar to mine, but much more elegant
+    //create the list..
+    const M = ["", "M", "MM", "MMM"];
+    const C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    //return solution with this procedure...
+    return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
+  */
 };
 
 const number = 58;
