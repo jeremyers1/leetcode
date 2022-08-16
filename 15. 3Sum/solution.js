a8d9355912c2710cc4966e3023c6355051b1c65d
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-08-16 12:39:31 -0700

    Final solution

--- 15. 3Sum/solution.js
+++ 15. 3Sum/solution.js
@@ -13,15 +13,20 @@ var threeSum = function (nums) {
 			else if (nums[first] + nums[second] + nums[last] > 0) last--;
 			// else they equal 0, so push result AND second++ and last--
 			else {
-				// but how to avoid duplicate entries? use filter method below...
+				// but how to avoid duplicate entries? use filter method below... from https://stackoverflow.com/questions/44014799/javascript-how-to-remove-duplicate-arrays-inside-array-of-arrays
+				// but my method doesn't doesn't work for LONG arrays with all 0s..
 				ans.push([nums[first], nums[second], nums[last]]);
+				// since array is sorted, we don't need to check duplicates
+				while (nums[first] === nums[first + 1]) first++;
+				while (nums[second] === nums[second + 1]) second++;
+				while (nums[last] === nums[last - 1]) last--;
 				second++;
 				last--;
 			}
 		}
 	}
-	// filter method from https://stackoverflow.com/questions/44014799/javascript-how-to-remove-duplicate-arrays-inside-array-of-arrays
-	return ans.filter(((t = {}), a => !(t[a] = a in t)));
+	// filter method
+	return ans;
 };
 
 let number = [-1, 0, 1, 2, -1, -4];
@@ -35,4 +40,6 @@ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 [0,1,-1] = [[0,1,-1]]
 
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.......VERY LONG] = [[0,0,0]]
+
+received GREAT help from here: https://leetcode.com/problems/3sum/discuss/2414590/javascript-solution
 */
