a858d15f841545116e220c84c0e6992a432046b9
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-09-22 09:39:37 -0700

    Add working solution

---
+++ 266. Palindrome Permutation/solution.js
@@ -0,0 +1,47 @@
+/*
+ * PREMIUM ONLY (so I wasn't able to look at the exact question)
+ * Given a string, write a function to check if it is a permutation of a palindrome.
+ * Time: populate object: O(N) + loop through object: O(N) = O(2N) = O(N)
+ * CTCI1.4
+ */
+
+function isPalindromPermutation(s) {
+	// spaces don't matter, so strip out or ignore spaces
+	let numOccurences = {};
+	let count = 0;
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] != ' ') {
+			count++;
+			numOccurences[s[i]] = numOccurences[s[i]] + 1 || 1;
+		}
+	}
+
+	console.log(count, numOccurences);
+
+	// in even-length string, all letters will have a even number of occurences; in odd-length, only one occurence will be odd
+	if (count % 2 === 0) {
+		console.log('even');
+		for (let key in numOccurences) {
+			if (numOccurences[key] % 2 != 0) {
+				return false;
+			}
+		}
+	} else {
+		console.log('odd');
+		let oneOdd = 0;
+		for (let key in numOccurences) {
+			if (numOccurences[key] % 2 != 0) {
+				oneOdd++;
+				if (oneOdd > 1) return false;
+			}
+		}
+	}
+	return true;
+}
+
+let str = 'tact coa';
+console.log(isPalindromPermutation(str));
+
+/* 
+tact coa = true (taco cat OR atca cta)
+*/
