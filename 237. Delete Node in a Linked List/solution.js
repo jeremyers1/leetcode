6890a10d283f49f59bccc1dc5d72301a7b8bcddb
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-10-15 10:58:27 -0700

    Add working solution, but then I realized I read instructions wrong. I ONLY have access to the node to be deleted.

---
+++ 237. Delete Node in a Linked List/solution.js
@@ -0,0 +1,65 @@
+/**
+ * There is a singly-linked list head and we want to delete a node node in it.
+ * You are given the node to be deleted node. You will not be given access to the first node of head.
+ * All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+ * Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+ *
+ * The value of the given node should not exist in the linked list.
+ * The number of nodes in the linked list should decrease by one.
+ * All the values before node should be in the same order.
+ * All the values after node should be in the same order.
+ *
+ * CTCI2.3
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+
+class ListNode {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
+
+function printList(head) {
+	let current = head;
+	let list = [];
+	while (current) {
+		list.push(current.data);
+		current = current.next;
+	}
+	console.log(list);
+}
+
+function deleteNode(node) {
+	let current = a;
+	let previous = null;
+	while (current && current.data != node) {
+		previous = current;
+		current = current.next;
+	}
+	previous.next = current.next;
+}
+
+let d = new ListNode(9);
+let c = new ListNode(1, d);
+let b = new ListNode(5, c);
+let a = new ListNode(4, b);
+printList(a);
+deleteNode(5);
+printList(a);
+
+/**
+ * [4,5,1,9], 5 = [4,1,9]
+ * [4,5,1,9], 1 = [4,5,9]
+ */
