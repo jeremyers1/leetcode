877cf9a0427d4296eef8d6c009a4db7a9cd0569b
JD Myers <4466264+jeremyers1@users.noreply.github.com>
2022-10-16 16:28:58 -0700

    Add solution that fits Leetcode challenge requirements

--- 1836. Remove Duplicates From an Unsorted Linked List/solution.js
+++ 1836. Remove Duplicates From an Unsorted Linked List/solution.js
@@ -1,6 +1,6 @@
 /*
  * Remove duplicates from an unsorted linked list
- * PREMIUM Leetcode Only
+ * PREMIUM Leetcode Only - on this problem, it is for ALL duplicates ... so if 3 is on there twice, BOTH 3s get removed
  * CTCI2.1
  */
 
@@ -14,8 +14,10 @@ class ListNode {
 function removeDups(listHead) {
 	const dups = {};
 	let current = listHead;
-	let previous = null;
-	while (current) {
+
+	/* Removes 2nd++ duplicates, but not first
+	 let previous = null;
+	 while (current) {
 		if (!dups[current.data]) {
 			dups[current.data] = current.data;
 			previous = current;
@@ -23,32 +25,58 @@ function removeDups(listHead) {
 			previous.next = current.next;
 		}
 		current = current.next;
+	} */
+
+
+	while (current) {
+		dups[current.data] ? dups[current.data]++ : (dups[current.data] = 1);
+		current = current.next;
 	}
+
+	// reset current
+	current = listHead;
+	let previous = new ListNode(0, current);
+	// find new head
+	while (current) {
+		if (dups[current.data] === 1) {
+			head = current;
+			break;
+		}
+		previous = current;
+		current = current.next;
+	}
+
+	// now move on from new head to find other duplicates
+	while (current) {
+		if (dups[current.data] > 1) {
+			previous.next = current.next;
+		} else {
+			previous = current;
+		}
+		current = current.next;
+	}
+	return head;
 }
 
 function printList(listHead) {
 	let current = listHead;
+	let print = [];
 	while (current) {
-		console.log(current.data);
+		print.push(current.data);
 		current = current.next;
 	}
+	console.log(print);
 }
 
-let d = new ListNode('d');
+let f = new ListNode('b');
+let e = new ListNode('a', f);
+let d = new ListNode('d', e);
 let c = new ListNode('c', d);
 let b = new ListNode('b', c);
 let a = new ListNode('a', b);
 
 printList(a);
 
-let e = new ListNode('a');
-d.next = e;
-let f = new ListNode('b');
-e.next = f;
-
-console.log('run 2');
-printList(a);
-
-console.log('run 3');
-removeDups(a);
-printList(a);
+console.log('remove dups?');
+// printList(removeDups(a));
+printList(removeDups(a));
